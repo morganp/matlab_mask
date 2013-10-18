8e6cc5591d7f7a30da1e02e0bb82d4a44a6b1500
Morgan Prior <morgan@amaras-tech.co.uk>
2013-10-18 12:52:11 +0100

    Added input options to allow setting of title and x/ylabels

--- package/+mask/mask.m
+++ package/+mask/mask.m
@@ -1,17 +1,53 @@
-function mask( datfile )
+function mask( datfile, varargin  )
+
+%% Input parsing example :
+% http://www.mathworks.co.uk/help/matlab/ref/inputparserclass.html
+    p = inputParser;
+    p.FunctionName = 'mask';
+    addOptional(p,'title','');
+    addOptional(p,'xlabel','');
+    addOptional(p,'ylabel','');
+parse(p, varargin{:} );
+
 
 %% Read in specified file
 maskdat = load( datfile );
 
-%%create line
+%% Create line
 
 
-%% generate plot
-figure()
-clf; % clear figure
-plot( maskdat(:,1), maskdat(:,2))
-hold on
-plot( maskdat(:,1), maskdat(:,3))
+ %% Generate plot
+ figure()
+ clf; % clear figure
+ plot( maskdat(:,1), maskdat(:,2))
+ hold on
+ plot( maskdat(:,1), maskdat(:,3))
+
+ % Find nice values for axis, so limits are visible
+ ymax_dat = max( [maskdat(:,2); maskdat(:,3)]);
+ ymin_dat = min( [maskdat(:,2); maskdat(:,3)]);
+ xmin     = 0;
+ xmax     = max( maskdat(:,1) );
+
+ resolution = 1;
+ try
+   % Try to round to nearest ten (10^resolution)
+   ymax = round_away_zeron(ymax_dat, resolution);
+   ymin = round_away_zeron(ymin_dat, resolution);
+ catch
+    disp('Requires the round_away_zeron function: please add https://github.com/morganp/matlab_roundn to your path');
+    ymax = ymax_dat;
+    ymin = ymin_dat;
+ end
+
+ %% yaxis values must be increasing and non-NaN, ie they cannot be the same.
+ if (ymin == ymax) ymin = ymin - 10^resolution; end
+
+ %axis([xmin xmax ymin ymax])
+ axis([xmin xmax ymin ymax]);
+ title(p.Results.title);
+ xlabel(p.Results.xlabel);
+ ylabel(p.Results.ylabel);
 
 end
 
